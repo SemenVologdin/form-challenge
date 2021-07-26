f809bf566a44ca486911c54149ab7e2921766c1d
SemenVologdin <thestrikesnake@gmail.com>
2021-07-26 10:42:41 +0600

    Make Header

---
+++ src/Step1.js
@@ -0,0 +1,12 @@
+import Typography from 'material-ui/styles/typography';
+import React from 'react';
+
+import { MainContainer } from './components/MainContainer';
+
+export const Step1 = () => {
+  return (
+    <MainContainer>
+      <Typography></Typography>
+    </MainContainer>
+  );
+};
